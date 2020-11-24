26488f8ff8035233859d723bad73b74e56395e34
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-25 02:47:52 +0530

    rectangle

--- src/components/index.js
+++ src/components/index.js
@@ -73,11 +73,12 @@ function relative(rec1,rec2) {
  const W = 100000; //total width
 
  function normalize(rec) {
-	 return
+	 return {
 	 x1:rec.top ? parseInt(rec.top):(T - (parseInt(rec.bottom) + parseInt(rec.height)));
 	 y1:rec.left ? parseInt(rec.left): (W - (parseInt(rec.right) + parseInt(rec.width)))
 	 x2:rec.bottom ? (T -parseInt(rec.bottom)): (parseInt(rec.top) + parseInt(rec.height));
 	 y2:rec.right? (W-parseInt(rec.right)): (parseInt(rec.left) + parseInt(rec.width));
- }
+   }
+}
 
 module.exports = updateStructure;
