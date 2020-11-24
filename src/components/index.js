cae02dfab157bba6a417ce5f895f8028c426a433
shivakumar901072 <shivakumar901072@gmail.com>
2020-11-25 02:18:41 +0530

    reactangle

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-//rec = {
+//rec = {()
 //	top: '25px',
 //	left: '96px',
 //	width: '64px',
@@ -6,7 +6,47 @@
 //}
 
 function updateStructure(rec1,rec2){
-	//write your code
+	//contains(1,2) returns true/false if 2 is contained in 1.
+	//relative(1,2) returns relative dimensions of 2 (w r t) to 1 
+	if(contains(rec1,rec2)) {
+		const relativeDim=relative(rec1,rec2);
+		return {...rec1,children:[relativeDim]};
+	}
+	else if (contains(rec2,rec1)) {
+		const relativedim = relative(rec2,rec1);
+		return {...rec2,children:[relativeDim]};
+	}
+	else {
+		return{...rec1};
+	}
 }
+
+ function contains(rec1,rec2) {
+	 const rec1n = normalize(rec1);
+	 const rec2n = normalize(rec2);
+
+	 if(
+		 rec1n.x1 <= rec2n.x1
+		&& rec1n.y1 <= rec2n.y1
+		&& rec1n.x2 >= rec2n.x2
+		&& rec1n.y2 >= rec2n.y2
+	 ) {
+		 return true;
+	 }
+	 return false;
+
+ }
+
+
+ const T = 100000;//total height
+ const W = 100000; //total width
+
+ function normalize(rec) {
+	 return
+	 x1:rec.top ? parseInt(rec.top):(T - (parseInt(rec.bottom) + parseInt(rec.height)));
+	 y1:rec.left ? parseInt(rec.left): (W - (parseInt(rec.right) + parseInt(rec.width)))
+	 x2:rec.bottom ? (T -parseInt(rec.bottom)): (parseInt(rec.top) + parseInt(rec.height));
+	 y2:rec.right? (W-parseInt(rec.right)): (parseInt(rec.left) + parseInt(rec.width));
+ }
 
 module.exports = updateStructure;
